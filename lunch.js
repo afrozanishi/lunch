96db216e80d3697f7f5115f95f58605fd456d0ab
afrozanishi <afroza1014@gmail.com>
2018-08-02 15:35:19 -0400

    Design Changes

---
+++ lunch.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var sand1 = document.getElementById('mondayImage');
+    sand1.addEventListener('click', function) (e) {
+        document.getElementById('mondayImage').src = "./images/sand.jpg"
+
+        if (document.getElementById('mondayImage').src === "./images/sandwich.jpg")
+    })
+} )
