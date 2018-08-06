7add3a1b85204acfc26a319f4aa201aa536ed344
afrozanishi <afroza1014@gmail.com>
2018-08-06 15:38:50 -0400

    my website

--- lunch.js
+++ lunch.js
@@ -1,8 +1,54 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    var sand1 = document.getElementById('mondayImage');
-    sand1.addEventListener('click', function) (e) {
-        document.getElementById('mondayImage').src = "./images/sand.jpg"
-
-        if (document.getElementById('mondayImage').src === "./images/sandwich.jpg")
+document.addEventListener('DOMContentLoaded', function (e) {
+    console.log("ITS LOADEDDDD");
+    var image = document.getElementById('mondayImage');
+    image.addEventListener('click', function (e) {
+        console.log("I just clicked on the sandwich image")
+        console.log(image.src) 
+        image.src = "file:///C:/Users/GWC15/Documents/whack/sand.jpg"
+    
     })
-} )
+})
+
+document.addEventListener('DOMContentLoaded', function (u) {
+    console.log("ITS LOADEDDDD");
+    var image = document.getElementById('tuesdayImage');
+    image.addEventListener('click', function (u) {
+        console.log("I just clicked on the taco image")
+        console.log(image.src) 
+        image.src = "file:///C:/Users/GWC15/Documents/whack/taco2.jpg"
+    
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function (q) {
+    console.log("ITS LOADEDDDD");
+    var image = document.getElementById('wednesdayImage');
+    image.addEventListener('click', function (q) {
+        console.log("I just clicked on the pasta image")
+        console.log(image.src) 
+        image.src = "file:///C:/Users/GWC15/Documents/whack/pasta2.jpg"
+    
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function (p) {
+    console.log("ITS LOADEDDDD");
+    var image = document.getElementById('thursdayImage');
+    image.addEventListener('click', function (p) {
+        console.log("I just clicked on the pizza image")
+        console.log(image.src) 
+        image.src = "file:///C:/Users/GWC15/Documents/whack/pizza3.jpg"
+    
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function (m) {
+    console.log("ITS LOADEDDDD");
+    var image = document.getElementById('fridayImage');
+    image.addEventListener('click', function (m) {
+        console.log("I just clicked on the random image")
+        console.log(image.src) 
+        image.src = "file:///C:/Users/GWC15/Documents/whack/random3.jpg"
+    
+    })
+})
